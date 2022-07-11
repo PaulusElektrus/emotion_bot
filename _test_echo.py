fbf8d4907806873439846ad8dc50a79ea0e677f9
Paul Zech <paul@zechs.de>
2022-07-11 16:04:42 +0200

    Added first echo test bot
    Added first echo test bot

---
+++ _test_echo.py
@@ -0,0 +1,18 @@
+import telebot
+import configparser
+
+config = configparser.ConfigParser()
+config.read_file(open(r'token.config'))
+token = config.get('config', 'token')
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
