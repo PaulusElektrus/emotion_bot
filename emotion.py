b4d8219a817ad589ff4e27a758fd2510e9f2d81b
Paul Z <58749158+Baule8520@users.noreply.github.com>
2022-07-27 14:27:20 +0200

    Added keyborad for answers

--- emotion.py
+++ emotion.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import telebot
-import configparser
-import time, threading, schedule
+from telebot import types
+import time, threading, schedule, configparser
 
 config = configparser.ConfigParser()
 config.read_file(open('./token.config', mode='r'))
@@ -21,6 +21,16 @@ def send_welcome(message):
 def beep(chat_id) -> None:
     """Send the beep message."""
     bot.send_message(chat_id, text='Wie geht es dir gerade?')
+    markup = types.ReplyKeyboardMarkup()
+    itembtna = types.KeyboardButton('1')
+    itembtnv = types.KeyboardButton('2')
+    itembtnc = types.KeyboardButton('3')
+    itembtnd = types.KeyboardButton('4')
+    itembtne = types.KeyboardButton('5')
+    itembtnf = types.KeyboardButton('6')
+    markup.row(itembtna, itembtnv, itembtnc)
+    markup.row(itembtnd, itembtne, itembtnf)
+    tb.send_message(chat_id, "Vergib eine Note für deine Selbstfürsorge:", reply_markup=markup)
 
 @bot.message_handler(commands=['erinnern'])
 def set_timer(message):
@@ -28,6 +38,7 @@ def set_timer(message):
     if len(args) == 1 and args[1].isdigit():
         sec = int(args[1])
         schedule.every(sec).minutes.do(beep, message.chat.id).tag(message.chat.id)
+        bot.reply_to(message, "Erinnerung gestellt auf ", sec, " Minuten :-)")
     else:
         bot.reply_to(message, 'Bitte im Format: /erinnern 5 (zum Beispiel)')
 
@@ -35,6 +46,7 @@ def set_timer(message):
 @bot.message_handler(commands=['stop'])
 def unset_timer(message):
     schedule.clear(message.chat.id)
+    bot.reply_to(message, "Erinnerung deaktiviert!")
 
 
 bot.message_handler(func=lambda message: True)
